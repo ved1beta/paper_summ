27722906180dd621864d6854a4e112265ca279c6
ved1beta <vedantthote2019@gmail.com>
2024-11-27 11:39:04 +0530

    rag implementation done writing modeular

--- src/data_processor.py
+++ src/data_processor.py
@@ -13,12 +13,18 @@ def text_formatter(text : str )->str:
 def open_read_pdf(pdf_path):
     doc = fitz.open(pdf_path)
     pages_text = []
-    for page , page_num in tqdm(enumerate(doc)):
+    for page_num in tqdm(range(len(doc))):
+        page = doc[page_num]
         text = page.get_text()
         text = text_formatter(text=text)
 
         pages_text.append({
-            "page_num":page_num,
-            "text":text,
-            "pages_token_count" : len(text)/4
-        })
+            "page_num": page_num,
+            "text": text,
+            "pages_token_count": len(text)/4
+        }) 
+    return pages_text
+pages_text = open_read_pdf("Assignment 3 material.pdf")
+print(pages_text[:10])
+
+
